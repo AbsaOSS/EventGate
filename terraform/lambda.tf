7f0252e65598470ddafe678deec5b3371adc6b48
Miroslav Chomut (CZ) <Miroslav.Chomut@absa.africa>
2025-05-14 10:22:42 +0200

    tf fix

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -11,6 +11,12 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol = "-1"
 }
 
+data "aws_s3_object" "event_gate_lambda_zip" {
+  count  = var.lambda_package_type == "Zip" ? 1 : 0
+  bucket = var.lambda_src_s3_bucket
+  key    = var.lambda_src_s3_key
+}
+
 resource "aws_lambda_function" "event_gate_lambda" {
   function_name = "${var.resource_prefix}event-gate-lambda"
   role = var.lambda_role_arn
@@ -21,7 +27,7 @@ resource "aws_lambda_function" "event_gate_lambda" {
   s3_bucket = var.lambda_package_type == "Zip" ? var.lambda_src_s3_bucket : null
   s3_key = var.lambda_package_type == "Zip" ? var.lambda_src_s3_key : null
   handler = var.lambda_package_type == "Zip" ? "event_gate_lambda.lambda_handler" : null
-  source_code_hash = var.lambda_package_type == "Zip" ? filebase64sha256("s3://${var.lambda_src_s3_bucket}/${var.lambda_src_s3_key}") : null
+  source_code_hash = var.lambda_package_type == "Zip" ? data.aws_s3_object.event_gate_lambda_zip[0].etag : null
   
   image_uri = var.lambda_package_type == "Image" ? var.lambda_src_ecr_image : null
   
