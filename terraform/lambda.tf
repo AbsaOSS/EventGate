c1a529c056d0eec57913ce9bab2943877773307b
Tobias.Mikula <tobias.mikula@absa.africa>
2025-10-16 11:47:06 +0200

    Bug fix.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "event_gate_sg" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.event_gate_sg.id
   cidr_ipv4 = "0.0.0.0/0"
-  ip_protocol = "-2"
+  ip_protocol = "-1"
 }
 
 data "aws_s3_object" "event_gate_lambda_zip" {
