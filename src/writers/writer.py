c3071e3c9083a2d7df040c21aca8d6494fe7ab01
Tobias.Mikula <tobias.mikula@absa.africa>
2026-01-14 11:49:58 +0100

    OOP approach for writer classes.

---
+++ src/writers/writer.py
@@ -0,0 +1,47 @@
+#
+# Copyright 2026 ABSA Group Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+This module provides abstract base class for all EventGate writers.
+"""
+
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Optional, Tuple
+
+
+class Writer(ABC):
+    """
+    Abstract base class for EventGate writers.
+    All writers inherit from this class and implement the write() method. Writers use lazy initialization.
+    """
+
+    def __init__(self, config: Dict[str, Any]) -> None:
+        self.config = config
+
+    @abstractmethod
+    def write(self, topic_name: str, message: Dict[str, Any]) -> Tuple[bool, Optional[str]]:
+        """
+        Publish a message to the target system.
+
+        Args:
+            topic_name: Target writer topic (destination) name.
+            message: JSON-serializable payload to publish.
+
+        Returns:
+            Tuple of (success: bool, error_message: Optional[str]).
+            - (True, None) on success or when writer is disabled/skipped.
+            - (False, "error description") on failure.
+        """
